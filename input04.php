6473d851ec599fef0fe18bcccfa38a03df124460
aryaya01 <79624714+aryaya01@users.noreply.github.com>
2022-06-11 15:01:46 +0700

    Add files via upload

---
+++ input04.php
@@ -0,0 +1,13 @@
+<html>
+    <head><title>Penolahan Form ~ Text</title></head>
+    <body>
+        <form action="proses04.php" method="POST" nama="input">
+            Sahabat-sahabat dekatku</br>
+            <input type="text" name="nama1"></br>
+            <input type="text" name="nama2"></br>
+            <input type="text" name="nama3"></br>
+            <input type="text" name="nama4"></br>
+            <input type="submit" name="input" value="input">
+        </form>
+    </body>
+</html>
---
+++ input05.php
@@ -0,0 +1,12 @@
+<html>
+    <head><title>Login Here</title></head>
+    <body>
+        <form action="proses05.php" method="POST" name="input">
+            <h2>Login Here...</h2>
+            Username: <input type="text" name="username"></br>
+            Password: <input type="password" name="password"></br>
+            <input type="submit" name="login" value="login">
+            <input type="reset" name="reset" value="reset">
+        </form>
+    </body>
+</html>
